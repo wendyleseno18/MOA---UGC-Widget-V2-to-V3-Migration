09637f1d9c80f403f6792530abbf7dcdd1486bce
WendyleChristianSeno <wendyle.seno@yotpo.com>
2025-07-01 18:25:15 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "1158015";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1158088";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1158087";
